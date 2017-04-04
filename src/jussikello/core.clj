4d8b2698ee15fe5a6b1fd8e950643800fb4e9a36
Riku Keski-Keturi <rihteri@gmail.com>
2017-04-04 20:53:13 +0300

    read some data from file instead of getting random sample

--- src/jussikello/core.clj
+++ src/jussikello/core.clj
@@ -3,17 +3,38 @@
   (:require [jussikello.spec :as js]
             [clojure.spec :as s]
             [clojure.test.check.generators :as gen]
-            [jussikello.magic :as magic]))
+            [jussikello.magic :as magic]
+            [clojure.string :as str]))
 
 (defn make-sample []
   (gen/sample (s/gen ::js/items) 2500))
 
+(defn read-sample [line]
+  (->> (-> line
+           (str/split #" "))
+       (mapv keyword)))
+
+(defn read-samples [filename]
+  (->> (-> filename
+           slurp
+           (str/split #"\n")
+           rest)
+       (map read-sample)))
+
+(defn calc-single-subset-result [[subset freqs]]
+  (-> freqs keys count))
+
+(defn calc-result-combos [result]
+  (->> result
+       (map calc-single-subset-result)
+       (apply +)))
+
 (defn tunkkaa [n]
-  (let [samples (make-sample)]
-    (time
-     (magic/calc-all-freqs samples n))))
+  (let [samples (read-samples "training.txt")]
+    (calc-result-combos
+     (time
+      (magic/calc-all-freqs samples n)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (tunkkaa))
+  (tunkkaa 3))
